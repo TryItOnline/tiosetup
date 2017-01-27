29eafb88b03b357c2bbd63b97ba48645da7aa27e
Andrew Savinykh <github.com@brutsoft.com>
2017-01-28 11:43:49 +1300

    work in progress

--- langdeps.sh
+++ langdeps.sh
@@ -2,7 +2,7 @@
 
 dnf install perl-CPAN npm java-1.?.0-openjdk nasm gcc-c++ clang julia glibc-devel.i686 libgcc.i686 pl ruby texi2html texinfo redhat-rpm-config python3-devel pcre-devel mercurial ant clisp clojure mono-devel dash -y
 npm install -g cheddar-lang coffee-script
-python3 -m pip install python-pcre
+python3 -m pip install exrex python-pcre
 PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC'
 wget http://downloads.dlang.org/releases/2.x/2.073.0/dmd-2.073.0-0.fedora.x86_64.rpm
 dnf install dmd-2.073.0-0.fedora.x86_64.rpm -y
