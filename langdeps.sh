3389309641e39a791fe36b658e0e6c3cb339f036
Andrew Savinykh <github.com@brutsoft.com>
2017-01-28 23:55:29 +1300

    work in progress

--- langdeps.sh
+++ langdeps.sh
@@ -7,13 +7,13 @@ dnf update -y
 dnf install allegro5-devel ant bison bison-devel clang clisp clojure cmake dash erlang fish flex flex-devel fsharp gcc-c++ gforth \
   glibc-devel.i686 golang groovy haskell-platform java-1.?.0-openjdk julia ksh libgcc.i686 mercurial mono-complete \
   nasm ncurses-compat-libs npm pcre-devel perl-CPAN pl python3-devel ruby zsh -y
+PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC'
 npm install -g cheddar-lang coffee-script babel-cli
 python3 -m pip install -upgrade pip
 python3 -m pip install exrex python-pcre hbcht sympy
 free && sync && echo 3 > /proc/sys/vm/drop_caches && free  
 python3 -m pip install numpy 
 free && sync && echo 3 > /proc/sys/vm/drop_caches && free  
-PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC'
 gem install treetop
 wget http://downloads.dlang.org/releases/2.x/2.073.0/dmd-2.073.0-0.fedora.x86_64.rpm
 dnf install dmd-2.073.0-0.fedora.x86_64.rpm -y
