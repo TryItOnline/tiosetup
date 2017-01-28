b9a28de7fa554e7ef40fdefa3ac2362d1754e5c1
Andrew Savinykh <github.com@brutsoft.com>
2017-01-29 10:47:04 +1300

    work in progress

--- langdeps.sh
+++ langdeps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+dnf install perl-CPAN -y
+PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC' >>/var/log/installdigestcrc 2>&1
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 dnf config-manager --add-repo http://download.mono-project.com/repo/centos/
 dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_24/shells:fish:release:2.repo
 dnf update -y
-dnf install perl-CPAN -y
-PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC'
 dnf install allegro5-devel ant bison bison-devel clang clisp clojure cmake dash erlang fish flex flex-devel fsharp gcc-c++ gforth \
   glibc-devel.i686 golang groovy haskell-platform java-1.?.0-openjdk julia ksh libgcc.i686 mercurial mono-complete \
   nasm ncurses-compat-libs npm pcre-devel perl-CPAN pl python3-devel ruby zsh -y
