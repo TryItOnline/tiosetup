4ef40f621a3c4e685f7d94c763dc86f88a44347f
Andrew Savinykh <github.com@brutsoft.com>
2017-01-29 00:12:32 +1300

    work in progress

--- langdeps.sh
+++ langdeps.sh
@@ -4,12 +4,13 @@ rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E032808
 dnf config-manager --add-repo http://download.mono-project.com/repo/centos/
 dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_24/shells:fish:release:2.repo
 dnf update -y
+dnf install perl-CPAN -y
+PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC'
 dnf install allegro5-devel ant bison bison-devel clang clisp clojure cmake dash erlang fish flex flex-devel fsharp gcc-c++ gforth \
   glibc-devel.i686 golang groovy haskell-platform java-1.?.0-openjdk julia ksh libgcc.i686 mercurial mono-complete \
   nasm ncurses-compat-libs npm pcre-devel perl-CPAN pl python3-devel ruby zsh -y
-PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC'
 npm install -g cheddar-lang coffee-script babel-cli
-python3 -m pip install -upgrade pip
+python3 -m pip install --upgrade pip
 python3 -m pip install exrex python-pcre hbcht sympy
 free && sync && echo 3 > /proc/sys/vm/drop_caches && free  
 python3 -m pip install numpy 
