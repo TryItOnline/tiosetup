e0fb4f8980140c837de9a0763964eb2936a18002
Andrew Savinykh <github.com@brutsoft.com>
2017-01-29 22:04:25 +1300

    work in progress

--- langdeps.sh
+++ langdeps.sh
@@ -9,15 +9,14 @@ dnf copr enable avsej/nim -y
 dnf update -y
 dnf install allegro5-devel ant bison bison-devel clang clisp clojure cmake dash erlang fish flex flex-devel fsharp gcc-c++ gforth \
   glibc-devel.i686 golang groovy haskell-platform java-1.?.0-openjdk julia ksh libgcc.i686 maxima mercurial mono-complete \
-  nasm ncurses-compat-libs nim npm octave octave-devel pcre-devel perl-CPAN pl python3-devel ruby zsh -y
+  nasm ncurses-compat-libs nim npm ocaml octave octave-devel pcre-devel perl-CPAN pl python3-devel ruby zsh -y
 npm install -g cheddar-lang coffee-script babel-cli
 python3 -m pip install --upgrade pip
 python3 -m pip install exrex python-pcre hbcht sympy
 python3 -m pip install numpy 
 python -m pip install sympy 
-#cp /usr/lib64/octave/4.0.3/mkoctfile-4.0.3 /usr/bin
-octave --eval "pkg install -forge symbolic"
-octave --eval "pkg rebuild -auto symbolic"
+octave --eval "pkg install -forge symbolic" >> /var/log/installoctave 2>&1
+octave --eval "pkg rebuild -auto symbolic" >> /var/log/installoctave 2>&1
 gem install treetop
 wget http://downloads.dlang.org/releases/2.x/2.073.0/dmd-2.073.0-0.fedora.x86_64.rpm
 dnf install dmd-2.073.0-0.fedora.x86_64.rpm -y
