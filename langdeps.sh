3ec0dd4f3a4d62ef010cb4d529ebfbf74d95d8aa
Andrew Savinykh <github.com@brutsoft.com>
2017-01-29 20:38:44 +1300

    work in progress

--- langdeps.sh
+++ langdeps.sh
@@ -5,14 +5,19 @@ PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Digest::CRC'
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 dnf config-manager --add-repo http://download.mono-project.com/repo/centos/
 dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/Fedora_24/shells:fish:release:2.repo
+dnf copr enable avsej/nim -y
 dnf update -y
 dnf install allegro5-devel ant bison bison-devel clang clisp clojure cmake dash erlang fish flex flex-devel fsharp gcc-c++ gforth \
-  glibc-devel.i686 golang groovy haskell-platform java-1.?.0-openjdk julia ksh libgcc.i686 mercurial mono-complete \
-  nasm ncurses-compat-libs npm pcre-devel perl-CPAN pl python3-devel ruby zsh -y
+  glibc-devel.i686 golang groovy haskell-platform java-1.?.0-openjdk julia ksh libgcc.i686 maxima mercurial mono-complete \
+  nasm ncurses-compat-libs nim npm octave octave-devel pcre-devel perl-CPAN pl python3-devel ruby zsh -y
 npm install -g cheddar-lang coffee-script babel-cli
 python3 -m pip install --upgrade pip
 python3 -m pip install exrex python-pcre hbcht sympy
 python3 -m pip install numpy 
+python -m pip install sympy 
+#cp /usr/lib64/octave/4.0.3/mkoctfile-4.0.3 /usr/bin
+octave --eval "pkg install -forge symbolic"
+octave --eval "pkg rebuild -auto symbolic"
 gem install treetop
 wget http://downloads.dlang.org/releases/2.x/2.073.0/dmd-2.073.0-0.fedora.x86_64.rpm
 dnf install dmd-2.073.0-0.fedora.x86_64.rpm -y
