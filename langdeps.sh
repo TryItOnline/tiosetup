d548fa4b1a7ec36d4749a19b993bd09bb134be66
Andrew Savinykh <github.com@brutsoft.com>
2017-01-30 09:37:41 +1300

    work in progress

--- langdeps.sh
+++ langdeps.sh
@@ -8,8 +8,9 @@ dnf config-manager --add-repo http://download.opensuse.org/repositories/shells:f
 dnf copr enable avsej/nim -y
 dnf update -y
 dnf install allegro5-devel ant bison bison-devel clang clisp clojure cmake dash erlang fish flex flex-devel fsharp gcc-c++ gforth \
-  glibc-devel.i686 golang groovy haskell-platform java-1.?.0-openjdk julia ksh libgcc.i686 maxima mercurial mono-complete \
-  nasm ncurses-compat-libs nim npm ocaml octave octave-devel pcre-devel perl-CPAN php-cli pl python3-devel rakudo ruby zsh -y
+  glibc-devel.i686 golang groovy haskell-platform icu java-1.?.0-openjdk julia ksh libgcc.i686 lldb lldb-devel lttng-tools lttng-ust \
+  maxima mercurial mono-complete nasm ncurses-compat-libs nim npm ocaml octave octave-devel pcre-devel perl-CPAN php-cli pl \
+  python3-devel rakudo ruby zsh -y
 npm install -g cheddar-lang coffee-script babel-cli
 python3 -m pip install --upgrade pip
 python3 -m pip install exrex python-pcre hbcht sympy
@@ -27,5 +28,19 @@ wget https://haskell.org/platform/download/8.0.2/haskell-platform-8.0.2-unknown-
 tar zxvf haskell-platform-8.0.2-unknown-posix--full-x86_64.tar.gz
 rm -f haskell-platform-8.0.2-unknown-posix--full-x86_64.tar.gz
 ./install-haskell-platform.sh
+cd ..
+rm -rf tmp
+mkdir -p tmp
+cd tmp
+wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/19/Everything/x86_64/os/Packages/l/libicu-50.1.2-5.fc19.x86_64.rpm
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.15/powershell-6.0.0_alpha.15-1.el7.centos.x86_64.rpm
+rpm2cpio libicu-50.1.2-5.fc19.x86_64.rpm | cpio -idmv
+dnf install powershell-6.0.0_alpha.15-1.el7.centos.x86_64.rpm -y
+rm -f powershell-6.0.0_alpha.15-1.el7.centos.x86_64.rpm
+rm -f libicu-50.1.2-5.fc19.x86_64.rpm
+mkdir -p /opt/microsoft/lib/
+cp usr/lib64/* /opt/microsoft/lib/
+cp aliases.ps1 /opt/microsoft/lib/
+rm -rf usr
 cd ..
 rm -rf tmp
