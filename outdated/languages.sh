07287558dfeeec613ad4eb5b11006cdaa40bb648
Andrew Savinykh <github.com@brutsoft.com>
2017-02-05 15:22:38 +1300

    adding log samples

--- outdated/languages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-./langdeps.sh
-mkdir -p /var/log/tioupd
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd /opt
-run-parts $DIR/languages
--- outdated/restore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-semanage fcontext -a -t bin_t '/srv/bin(/.*)?'
-semanage fcontext -a -t bin_t '/srv/wrappers(/.*)?'
-restorecon -Rv /home/runner/.ssh/authorized_keys
-restorecon -Rv /srv/bin
-restorecon -Rv /srv/wrappers
