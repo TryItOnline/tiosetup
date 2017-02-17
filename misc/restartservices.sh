6eeb1feefd586490577db34c4872020f3420688c
Andrew Savinykh <github.com@brutsoft.com>
2017-02-17 21:14:38 +1300

    cleaning up misc scripts

--- misc/restartservices.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-systemctl daemon-reload
-systemctl restart tiotests.timer
-systemctl restart tiotestsnz.timer
-systemctl restart frontail.service
-systemctl restart frontailnz.service
