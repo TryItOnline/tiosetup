19bb41765d84229c42ea6ab7a7e2a3b68581bce3
Andrew Savinykh <github.com@brutsoft.com>
2017-09-07 11:58:40 +1200

    keep experementing with .net core

--- files/dotnet/code.cs
+++ files/dotnet/code.cs
@@ -2,6 +2,7 @@ using System;
 using DynamicExpresso;
 class Program {
 	static void Main(string[] args) {
+                var interpreter = new Interpreter();
 		Console.WriteLine("Hello, World!");
 	}
 }
