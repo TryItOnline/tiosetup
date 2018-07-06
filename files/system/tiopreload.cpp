e069448d1242196b15aa44a281a86dda2b0b9b43
Dennis <dennis@tryitonline.net>
2018-07-06 16:36:56 +0000

    fix tiopreload (workarounds)

--- files/system/tiopreload.cpp
+++ files/system/tiopreload.cpp
@@ -1,11 +1,12 @@
-#include <cstdio>
 #include <cstdarg>
+#include <cstdio>
+#include <cstring>
 
 int __builtin_printf(const char *format, ...)
 {
 	va_list etc;
 	va_start(etc, format);
-	bool debug = format == "%s:%d: %s: Assertion '%s' failed.\n";
+	bool debug = !strcmp(format, "%s:%d: %s: Assertion '%s' failed.\n");
 	int retval = vfprintf(debug ? stderr : stdout, format, etc);
 	va_end(etc);
 
